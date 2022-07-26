c0d5257a17e15dfd680ba7a358894e3b865a7666
Lucas Coelho <lucas@listory.com>
2022-07-26 20:13:31 -0300

    Initial Commit

---
+++ Acronyms/AcronymsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcronymsApp.swift
+//  Acronyms
+//
+//  Created by Lucas Coelho on 26/07/22.
+//
+
+import SwiftUI
+
+@main
+struct AcronymsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
